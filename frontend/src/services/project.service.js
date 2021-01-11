7421f40e23c92fcd86ca8aa6051fc599d56b9bc2
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-11 22:57:56 +0100

    added create project service method

--- frontend/src/services/project.service.js
+++ frontend/src/services/project.service.js
@@ -1,8 +1,12 @@
-import { handleFetchResponse, BASE_API_URL, fetchOptions } from './common';
+import { handleFetchResponse, BASE_API_URL, fetchOptions, fetchOptionsPost } from './common';
 
 class ProjectService {
   getAll() {
     return fetch(`${BASE_API_URL}/project/get-all`, fetchOptions()).then(handleFetchResponse);
+  }
+
+  create(data) {
+    return fetch(`${BASE_API_URL}/project/create`, fetchOptionsPost(data)).then(handleFetchResponse);
   }
 }
 
