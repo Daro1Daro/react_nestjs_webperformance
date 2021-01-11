efac174ac17133c2d562e6814b6d71843ca53e5c
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-11 23:23:11 +0100

    added ListItemLink component

---
+++ frontend/src/components/list-item-link/list-item-link.component.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+
+const ListItemLink = (props) => (<ListItem button component="a" {...props} />);
+
+export default ListItemLink;
