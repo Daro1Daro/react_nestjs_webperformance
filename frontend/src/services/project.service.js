1e5b798f28c3b411ba7896e85e3ba1dbe1ef0549
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-15 22:14:36 +0100

    added delete project api method

--- frontend/src/services/project.service.js
+++ frontend/src/services/project.service.js
@@ -8,6 +8,18 @@ class ProjectService {
   create(data) {
     return fetch(`${BASE_API_URL}/project/create`, fetchOptionsPost(data)).then(handleFetchResponse);
   }
+
+  delete(data) {
+    return fetch(`${BASE_API_URL}/project/delete`, fetchOptionsPost(data)).then(handleFetchResponse);
+  }
+
+  createWebPage(data) {
+    return fetch(`${BASE_API_URL}/project/webpage/create`, fetchOptionsPost(data)).then(handleFetchResponse);
+  }
+
+  runSingleTest(data, webPageId) {
+    return fetch(`${BASE_API_URL}/project/webpage/${webPageId}/run-test`, fetchOptionsPost(data)).then(handleFetchResponse);
+  }
 }
 
 export default new ProjectService();
