16ac58c8a4e296786670fcf58bd842b1a67f1c86
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-18 20:18:14 +0100

    created ProjectPage component

---
+++ frontend/src/pages/project-page/project-page.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './project-page.styles.scss';
+
+class ProjectPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ProjectPage;
