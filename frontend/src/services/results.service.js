a295c5dd6a14d2be2657908f207814ae5db0a2e5
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-18 22:40:11 +0100

    added results API method for fetching results by id

--- frontend/src/services/results.service.js
+++ frontend/src/services/results.service.js
@@ -1,6 +1,10 @@
 import { handleFetchResponse, BASE_API_URL, fetchOptions, fetchOptionsPost } from './common';
 
 class ResultsService {
+  get(resultsId) {
+    return fetch(`${BASE_API_URL}/project/results/single/get/${resultsId}`, fetchOptions()).then(handleFetchResponse);
+  }
+
   getAllSingle() {
     return fetch(`${BASE_API_URL}/project/results/single/get-all`, fetchOptions()).then(handleFetchResponse);
   }
