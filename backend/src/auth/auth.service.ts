4b30b8ae2c38f3a7f6b031381a29ec97690361aa
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-22 23:02:49 +0100

    no message

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -45,8 +45,8 @@ export class AuthService {
       port: 587,
       secure: false,
       ignoreTLS: true,
-      requireTLS: false,
-      // tls: { rejectUnauthorized: false },
+      requireTLS: true,
+      tls: { rejectUnauthorized: false },
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
