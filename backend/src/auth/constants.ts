3dab8800254322e0085d69d228fff18747d20443
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-22 19:28:43 +0100

    no message

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import { join } from "path";
-dotenv.config({ path: join(__dirname, `${process.env.NODE_ENV === 'production' ? '' : '../..'}`, `.env.${process.env.NODE_ENV || 'dev'}`) });
+dotenv.config({ path: join(__dirname, `${process.env.NODE_ENV === 'production' ? '..' : '../..'}`, `.env.${process.env.NODE_ENV || 'dev'}`) });
 
 export const jwtConstants = {
   secret: process.env.JWT_SECRET,
