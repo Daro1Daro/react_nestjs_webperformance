69081940f7a9463aa96f365b9ef3250e2796e039
s203935 Dariusz Małagowski <203935@edu.p.lodz.pl>
2021-01-23 15:05:05 +0100

    no message

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -41,12 +41,13 @@ export class AuthService {
     const verifyUrl = `${process.env.APP_HOST}/activate/${token}`;
 
     let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      ignoreTLS: true,
-      requireTLS: true,
-      tls: { rejectUnauthorized: false },
+      // host: 'smtp.gmail.com',
+      // port: 587,
+      // secure: false,
+      // ignoreTLS: true,
+      // requireTLS: true,
+      // tls: { rejectUnauthorized: false },
+      service: 'Gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
